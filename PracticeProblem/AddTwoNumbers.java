dbe8314a278f23d1847cdba812dbb2db7a7e7dde
Abhishek-kumar873 <abhishek.kumar.01cse@gmail.com>
2026-08-25 12:13:10 +0530

    PracticeProblem

---
+++ PracticeProblem/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class AddTwoNumbers {
+    public static void main(String[] a) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println(x + y);
+
+        sc.close();
+    }
+}
